f2b0c3b6b6d4ea8fd4100941b822e7ab6b8a0ccd
dgwp-projects <zayneabraham1@gmail.com>
2017-04-11 20:47:06 -0400

    crepes

--- app/helpers/prizes_helper.rb
+++ app/helpers/prizes_helper.rb
@@ -1,2 +1,37 @@
 module PrizesHelper
+	def make_muh_slides(campaign, user)
+		# prize_count = campaign.prizes.count
+		active=false
+		tiers_li = campaign.prizes.order(:milestone).each_with_index.map do |camp, index|
+			# if index==0
+			# 	begin="<li class=>"
+			# else
+			# 	begin=""
+			# end
+		da_class = ""
+	
+			if user.score >= camp.milestone && campaign.prizes[index+1] && user.score <= campaign.prizes[index + 1].milestone
+				da_class = "active"
+				active=true
+			end
+
+			"<li class='#{da_class}' data-id='#{camp.milestone}'>#{camp.milestone} referrels</li>"
+		end
+		if active==false
+			da_class="active"
+		else
+			da_class=""
+		end
+		html = "<li class='#{da_class}' data-id='0'>0 referrels</li>" + tiers_li.join("")
+		
+		# byebug
+
+		return html.html_safe
+
+	end
+
 end
+
+#get campaign and count # of perks
+#find out how many points user has 
+#put slider at rounded down point on perks slider
--- app/helpers/subscribers_helper.rb
+++ app/helpers/subscribers_helper.rb
@@ -7,7 +7,17 @@ module SubscribersHelper
 		else
 			title="Thanks for Signing Up!"
 		end
-		
+		slider= make_muh_slides(sub.campaign, sub)
+		userscore=nil
+		sub.campaign.prizes.order(:milestone).each_with_index do |x, index|
+			if sub.score >= x.milestone && sub.campaign.prizes[index+1] && sub.score <= sub.campaign.prizes[index + 1].milestone
+				userscore=index+1
+			end
+		end
+		if userscore.nil?
+			userscore=0
+		end
+
 		html_output=<<-HTML
 		<div id="myModal" class="modal fade" role="dialog">
 		  <div class="modal-dialog">
@@ -20,6 +30,15 @@ module SubscribersHelper
 			    <p style="font-family:'Montserrat';font-weight:900px;font-size:22px;">Your Score:#{sub.score}</p>
 		        <p>Your Referral Code:<input style="display:inline;" class="form-control" readonly type="text" value="#{campaign_url}?referred_by=#{sub.referral_code}"/></p>
 		      </div>
+			  <div id="year-slider" class="no-ui-slider">
+			  	<span id="slider-start" style="display:none">#{userscore}</span>
+			  	<span id="slider-max" style="display:none">#{sub.campaign.prizes.count}</span>
+				  	<div class="slider"></div>
+				  		<ul class="slider-labels eleven-columns" id="sliderthing">
+						    #{slider}
+				  		</ul>
+					</div>
+			  </div>		     
 		      <div class="modal-footer">
 		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 		      </div>
